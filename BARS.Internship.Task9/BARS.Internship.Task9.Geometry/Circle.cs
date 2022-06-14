0c97052354be050e35b387d51170846273330924
Dmitriy <dimas.mc.123@gmail.com>
2022-06-14 19:02:50 +0300

    done task 9 (not full)

---
+++ BARS.Internship.Task9/BARS.Internship.Task9.Geometry/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BARS.Internship.Task9.Geometry
+{
+    public static class Circle
+    {
+        public static double Length(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public static double Area(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double Volume(double radius)
+        {
+            return (4.0 / 3) * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+}
---
+++ BARS.Internship.Task9/BARS.Internship.Task9.Geometry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace BARS.Internship.Task9.Geometry
+{
+    delegate double Calculate(double param);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            
+            try
+            {
+                radius = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            Calculate calculate = Circle.Length;
+            Console.WriteLine(calculate.Invoke(radius));
+            
+            calculate = Circle.Area;
+            Console.WriteLine(calculate.Invoke(radius));
+
+            calculate = Circle.Volume;
+            Console.WriteLine(calculate.Invoke(radius));
+        }
+    }
+}
